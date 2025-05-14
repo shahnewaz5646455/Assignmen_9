83c40f1cca1fc81ed7bcffe3ae514cc49ff0c876
shahnewaz5646455 <shahnewaz794@gmail.com>
2025-05-14 22:19:37 +0600

    update cart component

--- src/page/Cart.jsx
+++ src/page/Cart.jsx
@@ -10,15 +10,24 @@ export default function Cart() {
       <h1 className="text-3xl font-semibold py-5 text-center">
         ALL SUBSCRIPTION
       </h1>
-      <div className="flex justify-center items-center gap-5">
+      <div className="flex flex-wrap justify-center items-center gap-5">
         {mycart.length > 0 ? (
           mycart.map((item) => (
-            <div key={item.id}>
+            <div key={item.id} className="">
               <img
                 className="w-[300px] rounded-lg object-cover h-[200px]"
                 src={item.thumbnail}
                 alt={item.name}
               />
+              <a
+                href={item.link} // Use <a> tag for external links
+                target="_blank" // Open in a new tab
+                rel="noopener noreferrer" // Security best practice
+              >
+                <button className="btn mt-3 w-full bg-orange-400">
+                  Visit site
+                </button>
+              </a>
             </div>
           ))
         ) : (
